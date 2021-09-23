751e8a0529a8df5e7a7bfef9a4cdf3ee0fd7464d
TejoBurgboy <90248566+TejoBurgboy@users.noreply.github.com>
2021-09-23 22:03:51 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+var darkModeButton = document.querySelector(".darkmodebutton");
+var bodyElement = document.querySelector("body");
+var getal = 0;
+
+function darkLightMode() {
+    bodyElement.classList.toggle("darkmode");
+
+    if (getal == 1) {
+        document.getElementById("lightdarkmodeimage").src = "images/darkmode.png";
+        getal = 0;
+    } else {
+        document.getElementById("lightdarkmodeimage").src = "images/lightmode.png";
+        getal = 1;
+    };
+};
+
+darkModeButton.addEventListener("click", darkLightMode);
