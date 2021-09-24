d7a9e99aa5d62f815545df51aad25353dbed560f
TejoBurgboy <90248566+TejoBurgboy@users.noreply.github.com>
2021-09-24 16:15:27 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 // JavaScript Document
 var darkModeButton = document.querySelector(".darkmodebutton");
 var bodyElement = document.querySelector("body");
+var knopNieuwsMenu = document.querySelector(".NieuwsPaginaKnop");
+var nieuwsPaginaMenu = document.querySelector(".NieuwsPaginaMenu");
 var getal = 0;
 
 function darkLightMode() {
@@ -16,3 +18,9 @@ function darkLightMode() {
 };
 
 darkModeButton.addEventListener("click", darkLightMode);
+
+function hamburgerMenuNieuws(){
+	nieuwsPaginaMenu.classList.toggle("NieuwsPaginaMenuOpen");
+}
+
+knopNieuwsMenu.addEventListener("click", hamburgerMenuNieuws);
